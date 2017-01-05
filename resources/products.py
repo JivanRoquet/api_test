dc490edaa0d627603adc60e29b85e0f6059034a4
Jivan Roquet <jivan.roquet@gmail.com>
2017-01-05 03:35:48 +0200

    typing fixes

--- resources/products.py
+++ resources/products.py
@@ -1,5 +1,6 @@
 from flask_classful import FlaskView, route
 from webargs.flaskparser import use_args, use_kwargs
+from typing import Tuple, List, Dict
 
 from application import app, db
 from models.product import Product as ProductModel
@@ -16,7 +17,7 @@ class ProductsView(FlaskView):
     representations = {'application/json': utils.api_response}
 
     @route('/', methods=['GET'])
-    def index(self):
+    def index(self) -> Tuple[List[Dict], int]:
         """ Lists all products """
         products = ProductModel.query.all()
         data = [ProductSchema().dump(product).data for product in products]
@@ -24,7 +25,7 @@ class ProductsView(FlaskView):
 
 
     @route('/<id>', methods=['GET'])
-    def get(self, id: str) -> dict:
+    def get(self, id: str) -> Tuple[Dict, int]:
         """ Gets a particular product """
         product = ProductModel.query.get(id)
 
@@ -34,22 +35,22 @@ class ProductsView(FlaskView):
 
         else:
             # product not found
-            return "NotFound", 404
+            return {'error': 'NotFound'}, 404
 
 
     @route('/', methods=['POST'])
     @use_kwargs(products_args.POST)
-    def post(self, name: str, price: str) -> dict:
+    def post(self, name: str, price: str) -> Tuple[Dict, int]:
         """ Creates a new product """
         product = ProductModel(name=name, price=price)
         db.session.add(product)
         db.session.commit()
-        return "created {}".format(product.id), 201
+        return {'success': 'created {}'.format(product.id)}, 201
 
 
     @route('/<id>', methods=['PATCH'])
     @use_kwargs(products_args.PATCH)
-    def patch(self, id: str, **kwargs) -> dict:
+    def patch(self, id: str, **kwargs) -> Tuple[Dict, int]:
         """ Updates one or more fields of a particular product """
         product = ProductModel.query.get(id)
 
@@ -64,26 +65,26 @@ class ProductsView(FlaskView):
                 product.name = kwargs['price']
 
             db.session.commit()
-            return "updated {}".format(product.id), 201
+            return {'success': 'updated {}'.format(product.id)}, 201
 
         else:
             # product not found
-            return "NotFound", 404
+            return {'error': 'NotFound'}, 404
 
 
     @route('/<id>', methods=['DELETE'])
-    def delete(self, id) -> dict:
+    def delete(self, id) -> Tuple[Dict, int]:
         """ Deletes a particular product """
         product = ProductModel.query.get(id)
 
         if product is not None:
             db.session.delete(product)
             db.session.commit()
-            return "deleted {}".format(product.id), 200
+            return {'success': 'deleted {}'.format(product.id)}, 201
 
         else:
             # product not found
-            return "NotFound", 404
+            return {'error': 'NotFound'}, 404
 
 
 ProductsView.register(app, route_base='/v1/products/')
